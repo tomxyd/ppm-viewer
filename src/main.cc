e15799847cdaaf4d1b98ef094a4926155b243f09
Tolulope David <toludavid39@gmail.com>
2025-06-26 17:34:39 -0600

    Added GLFW, Glad, and ImGui Libraries into the project using CMake

---
+++ src/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+}
